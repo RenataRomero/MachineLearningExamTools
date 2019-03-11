d9d8a2739c0974d093fe8fb32ef5a38d09460cd8
reromero <reromero@compusoluciones.com>
2019-03-11 15:55:59 -0600

    Added error and the prediction with the help of linear regression

--- GaussJordan.py
+++ GaussJordan.py
@@ -37,5 +37,32 @@ def gauss(a, b):
 a = [[3, 4, -1], [0, -2, 10], [0, 4, -2]]
 b = [[-6],[-8], [-2]]
 det, c = gauss(a, b)
- 
+
+
+#Se imprime el vector b
 print(c)
+
+
+def predict_y(b, x):
+
+    y = b[0] + (b[1]*x[0]) + (b[2]*x[1]) + (b[3]*x[2]) + (b[4]*x[3]) + (b[5]*x[4])
+    return y
+
+#Obtiene el error con la formula
+def error(y_test, y_prediction):
+
+    y_yp_sum = 0
+
+    #Sumatoria en la raíz
+    for n in y_test:
+        dif = n - y_prediction
+        y_yp_sum += dif
+
+#Recorre cada una de las filas de la información de teste y hace la prediccion
+for n in filetest:
+    y_predictions += predict_y(b,n)
+
+#Una vez que se tienen todas las predicciones se calcula el error en base a el número real
+print(error(filetest_y, y_predictions)) #Se imprime el error
+
+
