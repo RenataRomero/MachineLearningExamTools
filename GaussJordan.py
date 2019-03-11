bbb3f2801dc205dfaf6d2224458e29f154ff547c
reromero <reromero@compusoluciones.com>
2019-03-11 12:02:16 -0600

    Gauss function

---
+++ GaussJordan.py
@@ -0,0 +1,43 @@
+import copy
+ 
+def gauss(a, b):
+    a = copy.deepcopy(a)
+    b = copy.deepcopy(b)
+    n = len(a)
+    p = len(b[0])
+    det = 1
+    for i in range(n - 1):
+        k = i
+        for j in range(i + 1, n):
+            if abs(a[j][i]) > abs(a[k][i]):
+                k = j
+        if k != i:
+            a[i], a[k] = a[k], a[i]
+            b[i], b[k] = b[k], b[i]
+            det = -det
+ 
+        for j in range(i + 1, n):
+            t = a[j][i]/a[i][i]
+            for k in range(i + 1, n):
+                a[j][k] -= t*a[i][k]
+            for k in range(p):
+                b[j][k] -= t*b[i][k]
+ 
+    for i in range(n - 1, -1, -1):
+        for j in range(i + 1, n):
+            t = a[i][j]
+            for k in range(p):
+                b[i][k] -= t*b[j][k]
+        t = 1/a[i][i]
+        det *= a[i][i]
+        for j in range(p):
+            b[i][j] *= t
+    return det, b
+ 
+# As an example, compute the determinant and inverse of 3x3 magic square
+ 
+a = [[2, 1, -3], [0, -2, 1], [0, 0, 1]]
+b = [[-10],[-2], [6]]
+det, c = gauss(a, b)
+ 
+print(c)
